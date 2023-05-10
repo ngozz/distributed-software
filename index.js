b3132ab71d6ca1c5a30978662e16a60389566c6c
Lê Duy Hiếu <hieu24052002@gmail.com>
2023-05-10 16:18:40 +0700

    js first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let client;
+
+//URL có hash (dấu thăng #) tức là đang ở trang nhận
+if (window.location.hash) { //chuyển từ trang gửi sang trang nhận khi có # trong url
+    document.getElementById('upPage').style.display = 'none';
+    document.getElementById('downPage').style.display = 'inline-block';
+}
